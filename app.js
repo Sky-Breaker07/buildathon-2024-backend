c24f83b2eacbf48a5c5296c722d316563c4be62c
FeranmiDev <macguzik98@gmail.com>
2024-09-07 23:20:00 +0100

    Refactor: Update routes and middleware for staff authentication

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 // Module imports
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 // File imports
-const connectDB = require('./db/conn');
-const patientRouter = require('./router/Patient');
-const assessmentTemplateRouter = require('./router/AssessmentTemplate');
-const staffRoutes = require('./routes/staff');
+const connectDB = require("./db/conn");
+const patientRouter = require("./router/Patient");
+const assessmentTemplateRouter = require("./router/AssessmentTemplate");
+const staffRouter = require("./routes/staff");
 
 // App Setup
 const app = express();
@@ -18,19 +18,19 @@ const PORT = process.env.PORT;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use('/api/v1/patients', patientRouter);
-app.use('/api/v1/assessment-template', assessmentTemplateRouter);
-app.use('/api/v1/staff', staffRoutes);
+app.use("/api/v1/patients", patientRouter);
+app.use("/api/v1/assessment-template", assessmentTemplateRouter);
+app.use("/api/v1/staff", staffRouter);
 
 const startServer = async () => {
-	try {
-		await connectDB(process.env.MONGO_URL);
-		app.listen(PORT, () =>
-			console.log(`Server started successfully on port: ${PORT}`)
-		);
-	} catch (error) {
-		console.error('Error starting server: ', error);
-	}
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(PORT, () =>
+      console.log(`Server started successfully on port: ${PORT}`)
+    );
+  } catch (error) {
+    console.error("Error starting server: ", error);
+  }
 };
 
 startServer();
