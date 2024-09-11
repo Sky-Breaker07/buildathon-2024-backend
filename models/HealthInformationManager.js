37974be5fd9a9b84655055c13c2f7cebca8cabdc
FeranmiDev <macguzik98@gmail.com>
2024-09-11 11:26:47 +0100

    Refactor: Add jwt dependency to HealthInformationManager model

--- models/HealthInformationManager.js
+++ models/HealthInformationManager.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const healthInformationManagerSchema = new mongoose.Schema({
     firstName: {
