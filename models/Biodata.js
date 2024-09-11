93645db241bc12ddc013f11888c3e64b01bd9755
FeranmiDev <macguzik98@gmail.com>
2024-09-11 19:03:12 +0100

    Refactor: Update enum values for sex and marital_status in Biodata model

--- models/Biodata.js
+++ models/Biodata.js
@@ -16,7 +16,7 @@ const biodataSchema = new mongoose.Schema(
 
 		sex: {
 			type: String,
-			enum: ['Male', 'Female'],
+			enum: ['male', 'female'],
 			required: true,
 		},
 
@@ -40,7 +40,7 @@ const biodataSchema = new mongoose.Schema(
 
 		marital_status: {
 			type: String,
-			enum: ['Single', 'Married', 'Divorced', 'Widowed'],
+			enum: ['single', 'married', 'divorced', 'widowed'],
 			required: true,
 		},
 
